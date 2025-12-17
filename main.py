d97e6113bf6e5e4e653234c8fb7386f0fefb81bc
sotois <kofiagyapong837@gmail.com>
2025-12-17 18:54:41 -0500

    "Version 2: Created computer choice function

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 def get_user_choice():
   choice = input("Enter rock, paper, or scissors: ")
   return choice
+
+def get_computer_choice():
+    computer_move = random.choice(['rock', 'paper', 'scissors'])
+    return computer_move
