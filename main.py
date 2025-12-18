0c63e9acaf4073db2212be83f9dfb6cf699168f8
sotois <kofiagyapong837@gmail.com>
2025-12-17 20:25:34 -0500

    Add function to determine winner in rock-paper-scissors

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
 def get_user_choice():
   choice = input("Enter rock, paper, or scissors: ")
   return choice
+def determine_winner(user_choice, computer_choice):
+  if user_choice == computer_choice:
+    return "It's a tie!"
+  elif user_choice == "rock":
+    if computer_choice == "scissors":
+      return "User wins!"
+  else:
+    return "Computer wins!"
+  elif user_choice == "paper":
+    if computer_choice == "rock":
+      return "User wins!"
+  else: 
+    return "Computer wins!"
+  elif user_choice == "scissors":
+    if computer_choice == "paper":
+      return "User wins!"
+  else:
+    return "Computer wins!"
